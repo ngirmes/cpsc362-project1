3375e1e3e8c9a450c69677f29f63460c0845e90c
ngirmes <n.girmes@csu.fullerton.edu>
2022-09-01 14:41:00 -0700

    Wrote code for login/create account

--- library.cpp
+++ library.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(){
     int user_choice = 0;
-    cout << "Welcome to Bookworm!\n";
-    cout << "1. Login \n";
-    cout << "2. Create an account.\n";
+    string username, password;
+
+    cout << "Welcome to Bookworm!\n1. Login\n2. Create an account.\n";
     cin >> user_choice ;
+    
+    if(user_choice == 1){
+        cout << "Enter your existing username: ";
+        cin >> username;
+        cout << "Enter your existing password: ";
+        cin >> password;
+    }
+    else if (user_choice == 2){
+        ofstream user_login_info;
+        user_login_info.open("user_login_info.txt", ios::app);
+        cout << "Enter a username: ";
+        cin >> username;
+        cout << "Enter a password: ";
+        cin >> password;
+        user_login_info << username + "\n";
+        user_login_info << password + "\n";
+        user_login_info.close();
+
+    }    
 
     return 0;
 }
