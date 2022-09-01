ed9477d2374fbb5ab745a57715a5eb034850877b
ngirmes <n.girmes@csu.fullerton.edu>
2022-09-01 14:12:47 -0700

    Began to write software for Bookworm

--- library.cpp
+++ library.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World!";
+    int user_choice = 0;
+    cout << "Welcome to Bookworm!\n";
+    cout << "1. Login \n";
+    cout << "2. Create an account.\n";
+    cin >> user_choice ;
+
     return 0;
 }
